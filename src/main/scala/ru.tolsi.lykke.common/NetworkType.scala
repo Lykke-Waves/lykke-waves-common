646499cdf0b93cc8e810b9498bb687601a778565
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-02-12 14:33:41 +0300

    network type serialization

--- src/main/scala/ru.tolsi.lykke.common/NetworkType.scala
+++ src/main/scala/ru.tolsi.lykke.common/NetworkType.scala
@@ -1,11 +1,31 @@
 package ru.tolsi.lykke.common
 
+import play.api.libs.json.{JsError, JsSuccess, JsValue, Reads}
+
+import scala.util.Try
+
 object NetworkType {
 
-  case object Test extends NetworkType
+  def fromString(s: String): NetworkType = s match {
+    case Test.name => Test
+    case Main.name => Main
+  }
 
-  case object Main extends NetworkType
+  case object Test extends NetworkType {
+    override val name: String = "test"
+  }
 
+  case object Main extends NetworkType {
+    override val name: String = "main"
+  }
+
+  implicit val networkTypeReader: Reads[NetworkType] =
+    (json: JsValue) =>
+      Try(NetworkType.fromString(json.as[String]))
+        .map(JsSuccess(_))
+        .getOrElse(JsError(s"Network type should be a string '${Test.name}' or '${Main.name}'"))
 }
 
-sealed trait NetworkType
+sealed trait NetworkType {
+  def name: String
+}
