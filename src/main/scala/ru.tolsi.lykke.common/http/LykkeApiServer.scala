bb63a64ae9592f71b615f28204e7bcaa21e65e5b
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-04-19 17:32:01 +0300

    added logging the errors

--- src/main/scala/ru.tolsi.lykke.common/http/LykkeApiServer.scala
+++ src/main/scala/ru.tolsi.lykke.common/http/LykkeApiServer.scala
@@ -2,14 +2,17 @@ package ru.tolsi.lykke.common.http
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.{Directives, RejectionHandler, Route, ValidationRejection}
+import com.typesafe.scalalogging.StrictLogging
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
 import play.api.libs.json.Json
 
 import scala.util.control.NonFatal
 
-trait LykkeApiServer extends Directives with PlayJsonSupport {
+trait LykkeApiServer extends Directives with PlayJsonSupport with StrictLogging {
   private val jsonRejectionHandler = RejectionHandler.newBuilder().handle {
-    case ValidationRejection(error, Some(NonFatal(t))) => complete(StatusCodes.BadRequest -> Json.toJson(ErrorMessage(error)))
+    case ValidationRejection(error, Some(NonFatal(t))) =>
+      logger.error("Error on request validation", t)
+      complete(StatusCodes.BadRequest -> Json.toJson(ErrorMessage(error)))
   }.result()
 
   def handleRejections(r: Route): Route = handleRejections(jsonRejectionHandler) {
